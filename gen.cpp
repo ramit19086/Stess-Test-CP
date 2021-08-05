4b2529720d4f1e8684cdce647ae18c6dbadbb707
ramit19086 <58386250+ramit19086@users.noreply.github.com>
2021-08-05 14:44:37 +0530

    Update gen.cpp

--- gen.cpp
+++ gen.cpp
@@ -1,20 +1,26 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+#define nl << '\n'
+#define ll long long
 
-int randint(int a, int b){
-    return a + rand() % (b - a + 1);
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+ 
+ll randint(ll l, ll r) {
+    return uniform_int_distribution<ll>(l, r)(rng);
+}
+char randchar(char a, char b)
+{
+    return (char)a + randint(0, b - a);
 }
 
-char randchar(char a, char b){
-    return (char) a + randint(0,b-a);
-}
-
-int main(int argc, char* argv[]){
-    srand(stoi(argv[1]));
-    int t = 1;
-    int a =randint(1,20);
-    int b =randint(a,a+20);
-    int v = randint(1,40);
-    cout<<t<<" "<<a<<" "<<b<<" "<<v <<"\n";
+int main(int argc, char *argv[])
+{
+    srand(atoi(argv[1]));/
+    cout << 1 nl;
+    ll n = randint(1,10);
+    cout << n nl;
+    for(int i = 0; i < n; i++)
+        cout << randint(1,20) <<" ";
+    cout nl;
     return 0;
 }
