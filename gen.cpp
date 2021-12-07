8fa8a3829a1e64f64877c95fa4adf1038ce43232
ramit19086 <58386250+ramit19086@users.noreply.github.com>
2021-12-07 15:01:50 +0530

    Update gen.cpp

--- gen.cpp
+++ gen.cpp
@@ -4,23 +4,71 @@ using namespace std;
 #define ll long long
 
 mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
- 
-ll randint(ll l, ll r) {
+
+ll randint(ll l, ll r)
+{
     return uniform_int_distribution<ll>(l, r)(rng);
 }
 char randchar(char a, char b)
 {
     return (char)a + randint(0, b - a);
 }
-
-int main(int argc, char *argv[])
+class DSU
 {
-    srand(atoi(argv[1]));/
-    cout << 1 nl;
-    ll n = randint(1,10);
-    cout << n nl;
-    for(int i = 0; i < n; i++)
-        cout << randint(1,20) <<" ";
-    cout nl;
-    return 0;
+private:
+    int n;
+    vector<int> rank, parent, size;
+public:
+    DSU(int n = 1) : n(n)
+    {
+        rank = vector<int>(n + 1), parent = vector<int>(n + 1), size = vector<int>(n + 1, 1);
+        for (int i = 1; i <= n; i++)
+            parent[i] = i;
+    }
+    int Find(int v)
+    {
+        if (v == parent[v])
+            return v;
+        return parent[v] = Find(parent[v]);
+    }
+
+    bool Union(int a, int b)
+    {
+        a = Find(a), b = Find(b);
+        if (a == b)
+            return 0;
+        if (rank[a] < rank[b])
+            swap(a, b);
+        parent[b] = a;
+        if (rank[a] == rank[b])
+            rank[a]++;
+        size[a] += size[b];
+        return 1;
+    }
+    int Component_size(int a)
+    {
+        return size[Find(a)];
+    }
+};
+
+vector<pair<int,int>> generate_tree(int n)
+{
+    DSU nodes(n);
+    vector<int> parent(n + 1);
+    for(int i = 2; i <= n; i++)
+    {
+        do
+        {
+            parent[i] = randint(1, n);
+        } while (!nodes.Union(parent[i], i));     
+    }
+    vector<pair<int,int>> edges;
+    for(int i = 2; i <= n; i++)
+        edges.push_back({parent[i], i});
+    return edges;
+}
+
+int main(int argc, char* argv[])
+{
+    
 }
